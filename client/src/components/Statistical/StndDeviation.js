a5a5dad417497acc0ac9b7f102ac27ef76c583f5
Ritika Talukdar <ritika.talukdar24@gmail.com>
2022-01-30 12:47:33 +0530

    added stat graphs

---
+++ client/src/components/Statistical/StndDeviation.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./style.css";
+import Plot from "react-plotly.js";
+import PlotJson from "../../scripts/plot.json";
+
+const StndDeviation = () => {
+  
+    function deviation(arr){
+        let mean = arr.reduce((acc, curr)=>{ return parseFloat(acc) + parseFloat(curr) }, 0) / arr.length;
+        arr = arr.map((k)=>{
+          return (k - mean) ** 2
+        })
+       let sum = arr.reduce((acc, curr)=> acc + curr, 0);
+
+       return Math.sqrt(sum / arr.length)
+    }
+
+  console.log(deviation(PlotJson.o));
+
+  return (
+    <div className="avg-container">
+       { <Plot
+        data={[
+          {
+            x: ["Voltage", "Current", "Power factor", "Power", "Energy"],
+            y: [
+                deviation(PlotJson.V),
+                deviation(PlotJson.o),
+                deviation(PlotJson.l),
+                deviation(PlotJson.t),
+                deviation(PlotJson.a),
+            ],
+            type: "bar",
+          },
+        ]}
+        layout={{
+          width: 330,
+          height: 240,
+          title: "Standard Deviation plot",
+        }}
+      /> } 
+    </div>
+  );
+};
+
+export default StndDeviation;
--- client/src/pages/StatsData/index.js
+++ client/src/pages/StatsData/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import AveragePlot from "../../components/Statistical/AveragePlot";
+import StndDeviation from "../../components/Statistical/StndDeviation";
+import VariancePlot from "../../components/Statistical/VariancePlot";
 import "./style.css";
 const index = () => {
   return (
   <div className="statplot-cards">
     <AveragePlot />
+    <VariancePlot />
+    <StndDeviation />
   </div>
   );
 };
